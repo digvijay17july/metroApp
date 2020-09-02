62e38915f97673b504a4c671364fa5dc6571e865
unknown <digvijay17july@gmail.com>
2020-09-03 03:51:29 +0530

    updated swagger

--- src/main/java/com/metroApp/MetroAppApplication.java
+++ src/main/java/com/metroApp/MetroAppApplication.java
@@ -1,13 +1,17 @@
 package com.metroApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MetroAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MetroAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MetroAppApplication.class, args);
+    }
 
 }
